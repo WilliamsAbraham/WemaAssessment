b1df41c52c763ec27d11f842928f891a0720a44f
Williams Abraham <wabraham@vatebra.com>
2025-03-22 20:00:27 +0100

    updated

--- WemaAssessment/CustomerServiceApi/Core/Domain/Customer.cs
+++ WemaAssessment/CustomerServiceApi/Core/Domain/Customer.cs
@@ -4,10 +4,6 @@ namespace CustomerServiceApi.Core.Domain
 {
     public class Customer : IdentityUser<Guid>
     {
-        //public Guid Id { get; set; }
-        //public string PhoneNumber { get; set; } = string.Empty;
-        //public string Email { get; set; } = string.Empty;
-        //public string PasswordHash { get; set; } = string.Empty;
         public string StateOfResidence { get; set; } = string.Empty;
         public string LGA { get; set; } = string.Empty;
         public bool IsVerified { get; set; }
